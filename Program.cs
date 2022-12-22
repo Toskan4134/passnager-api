223475b030bb7cae732689c261d682e45f33f1b0
a25106 <a25106@svalero.com>
2022-12-22 09:26:47 +0100

    Added Database Connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
+
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+if (Environment.GetEnvironmentVariable("DB_PASSWORD") != null)
+{
+    builder.Configuration["DBPassword"] = Environment.GetEnvironmentVariable("DB_PASSWORD");
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var conStrBuilder = new NpgsqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
+conStrBuilder.Password = builder.Configuration["DBPassword"];
+builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(conStrBuilder.ConnectionString));
+
+builder.Services.AddTransient<DataContext>();
+
+
+builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: "frontendOrigin",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:5500").AllowAnyHeader().AllowAnyMethod();
+                    });
+            });
 
 var app = builder.Build();
 
@@ -15,6 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("frontendOrigin");
 
 app.UseHttpsRedirection();
 
