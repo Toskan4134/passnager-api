5cab6c7bd0a7ba6db9c116a6f2e402537462730a
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-01-17 20:59:18 +0100

    Changed Conn String

--- Program.cs
+++ Program.cs
@@ -9,12 +9,16 @@ if (Environment.GetEnvironmentVariable("DB_PASSWORD") != null)
 {
     builder.Configuration["DBPassword"] = Environment.GetEnvironmentVariable("DB_PASSWORD");
 }
+if (Environment.GetEnvironmentVariable("DB_CONN_STRING") != null)
+{
+    builder.Configuration["ConnectionString"] = Environment.GetEnvironmentVariable("DB_CONN_STRING");
+}
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var conStrBuilder = new NpgsqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
+var conStrBuilder = new NpgsqlConnectionStringBuilder(builder.Configuration["ConnectionString"]);
 conStrBuilder.Password = builder.Configuration["DBPassword"];
 builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(conStrBuilder.ConnectionString));
 
--- README.md
+++ README.md
@@ -27,3 +27,6 @@ CREATE TABLE "Site" (
   "IsActive" BOOLEAN NOT NULL
 );
 ```
+docker build --rm -t passnager-api/agaudes:latest .
+
+docker run --rm -p 5106:5106 -p 5107:5107 -e ASPNETCORE_HTTP_PORT=https://+:5107 -e ASPNETCORE_URLS=http://+:5106 -e DB_CONN_STRING="Server=localhost;Database=postgres;Username=postgres;Port=5432" passnager-api/agaudes
